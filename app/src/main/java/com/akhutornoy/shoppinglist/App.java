fb452a8af3e653d19749d3f2ddf290213f6bbe25
akhutornoy <alexander.khutornoy@dataart.com>
2018-06-09 22:23:36 +0300

    - upgrade compileSDK to 27, and all dependent libraries  - SQLScout - SQLite view enabled

--- app/src/main/java/com/akhutornoy/shoppinglist/App.java
+++ app/src/main/java/com/akhutornoy/shoppinglist/App.java
@@ -2,10 +2,13 @@ package com.akhutornoy.shoppinglist;
 
 import android.app.Application;
 
+import com.idescout.sql.SqlScoutServer;
+
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        SqlScoutServer.create(this, getPackageName());
     }
 }
