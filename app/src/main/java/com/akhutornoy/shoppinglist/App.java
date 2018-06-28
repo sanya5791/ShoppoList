cabeaef9d757b31edcd9cdc93310b95c31562502
akhutornoy <alexander.khutornoy@dataart.com>
2018-06-28 11:37:10 +0300

    - SQLScout is replaced with Stetho  - libraries update

--- app/src/main/java/com/akhutornoy/shoppinglist/App.java
+++ app/src/main/java/com/akhutornoy/shoppinglist/App.java
@@ -2,13 +2,12 @@ package com.akhutornoy.shoppinglist;
 
 import android.app.Application;
 
-import com.idescout.sql.SqlScoutServer;
-
+import com.facebook.stetho.Stetho;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SqlScoutServer.create(this, getPackageName());
+        Stetho.initializeWithDefaults(this);
     }
 }
